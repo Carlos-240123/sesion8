be4b8103a7164c2ca189652fde5abde4c4e6d992
Carlos-240123 <123522081+Carlos-240123@users.noreply.github.com>
2025-10-28 10:44:05 -0500

    Update economia.php

--- noticiero/api/secciones/economia.php
+++ noticiero/api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+ "titulo" => "Economía peruana crece un 5% en el último trimestre",
+    "autor" => "Agencia Andina",
+    "resumen" => "El crecimiento fue impulsado por exportaciones mineras y turismo.",
 ];
 ?>
