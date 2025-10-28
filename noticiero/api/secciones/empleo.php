f1b0517cd9eb426d7d1b3c94093b6aac998da0ac
Carlos-240123 <123522081+Carlos-240123@users.noreply.github.com>
2025-10-28 10:44:24 -0500

    Update empleo.php

--- noticiero/api/secciones/empleo.php
+++ noticiero/api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "Nuevos programas de empleo juvenil en marcha",
+    "autor" => "Ministerio de Trabajo",
+    "resumen" => "Más de 50 mil jóvenes accederán a oportunidades laborales en distintas regiones.",
 ];
 ?>
