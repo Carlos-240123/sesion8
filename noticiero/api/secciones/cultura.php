a06754956460fb49c0b07699c97ff7117b1dff05
Carlos-240123 <123522081+Carlos-240123@users.noreply.github.com>
2025-10-28 10:43:08 -0500

    Update cultura.php

--- noticiero/api/secciones/cultura.php
+++ noticiero/api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "Festival cultural reúne a miles de artistas en Lima",
+    "autor" => "Cultura Perú",
+    "resumen" => "El festival anual celebra la diversidad cultural con música, danza y exposiciones.",
 ];
 ?>
