183e513910448014edc0b2f82edb6ce1a241d2da
Carlos-240123 <123522081+Carlos-240123@users.noreply.github.com>
2025-10-28 10:43:47 -0500

    Update deportes.php

--- noticiero/api/secciones/deportes.php
+++ noticiero/api/secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La selección peruana gana en un partido histórico",
+    "autor" => "Deportes TV",
+    "resumen" => "Perú venció 3-1 a su rival en un encuentro vibrante lleno de emoción.",
 ];
 ?>
