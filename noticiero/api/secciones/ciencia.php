9e5ca4e8622e80d96ae2b3e95ee8ee5ec08867dd
Carlos-240123 <123522081+Carlos-240123@users.noreply.github.com>
2025-10-28 10:41:28 -0500

    Update ciencia.php

--- noticiero/api/secciones/ciencia.php
+++ noticiero/api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubren nuevo material que podría revolucionar la energía solar",
+"autor" => "Revista Científica Global",
+"resumen" => "Investigadores desarrollaron un material que aumenta un 40% la eficiencia de paneles solares.",
 ];
 ?>
